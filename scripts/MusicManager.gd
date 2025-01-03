a741c80a78d7e0a17a5d01ee9c340c6117b04c1b
FENET <productivefenetmuler@gmail.com>
2025-01-03 21:26:30 +0300

    Add MusicManager script for music playback control

---
+++ scripts/MusicManager.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@onready var music_player = AudioStreamPlayer.new()
+var current_track: String = ""
+
+func _ready():
+    add_child(music_player)
+    
+    # music_player.process_mode = Node.PROCESS_MODE_ALWAYS
+
+func play_music(audio_path: String):
+    if current_track == audio_path and music_player.playing:
+        return
+        
+    current_track = audio_path
+    var stream = load(audio_path)
+    music_player.stream = stream
+    
+func stop_music():
+    music_player.stop()
+    
+func pause_music():
+    music_player.stream_paused = true
+    
+func resume_music():
+    music_player.stream_paused = false
