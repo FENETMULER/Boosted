3f104512266b93093234cac37056589d22f3fb33
FENET <productivefenetmuler@gmail.com>
2025-01-08 18:11:21 +0300

    Create PauseMenu script

---
+++ scripts/PauseMenu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().paused = false
+	OverlayManager.pop_overlay()
+
+
+func _on_main_menu_button_pressed() -> void:
+	_change_scene_and_pop_overlay('main_menu')
+
+
+func _on_tracks_button_pressed() -> void:
+	_change_scene_and_pop_overlay('track_selection_menu')
+
+func _change_scene_and_pop_overlay(scene_name: String) -> void:
+	get_tree().paused = false
+	SceneManager.change_scene(scene_name)
+	OverlayManager.pop_overlay()
