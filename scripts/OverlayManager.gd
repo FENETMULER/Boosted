bf88cde94576c51c981ea1c4bc1712237e17a385
FENET <productivefenetmuler@gmail.com>
2025-01-08 18:09:44 +0300

    Improve OverlayManager functionality

--- scripts/OverlayManager.gd
+++ scripts/OverlayManager.gd
@@ -1,11 +1,28 @@
 extends CanvasLayer
 
+var overlay_stack: Array = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var overlay_scenes: Dictionary = {
+	"pause_menu": preload("res://scenes/ui/PauseMenu.tscn"),
+	# "Leaderboard": preload("res://scenes/Leaderboard.tscn"),
+	
+}
 
+func push_overlay(overlay_name: String):
+	var overlay = overlay_scenes[overlay_name].instantiate()
+	overlay_stack.append(overlay)
+	add_child(overlay)
+	overlay.position = Vector2(0, 0) # Adjust to your screen's center or desired position
+	# overlay.process_mode = Node.PROCESS_MODE_ALWAYS
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Removes the top overlay from the stack
+func pop_overlay():
+	if overlay_stack.size() > 0:
+		var overlay = overlay_stack.pop_back()
+		overlay.queue_free()
+
+# Clears all overlays
+func clear_overlays():
+	for overlay in overlay_stack:
+		overlay.queue_free()
+	overlay_stack.clear()
