048e43ca281a36277f2bf0eea6811ef67d52c482
FENET <productivefenetmuler@gmail.com>
2025-01-13 19:08:33 +0300

    Fix always playing music bug

--- scripts/MusicManager.gd
+++ scripts/MusicManager.gd
@@ -1,26 +1,41 @@
 extends Node
 
 @onready var music_player = AudioStreamPlayer.new()
-var current_track: String = ""
+var current_music_track: String = ""
+var music_enabled: bool
 
 func _ready():
-    add_child(music_player)
-    
-    # music_player.process_mode = Node.PROCESS_MODE_ALWAYS
+	process_mode = PROCESS_MODE_ALWAYS
+	add_child(music_player)
+	
+	var config = ConfigFile.new()
+	if config.load("user://settings.cfg") == OK:
+		music_enabled = config.get_value("audio", "music_enabled", true)
+		set_music_enabled(music_enabled)
+
+func set_music_enabled(enabled: bool) -> void:
+	music_enabled = enabled
+	if music_enabled:
+		resume_music()
+	else:
+		pause_music()
 
 func play_music(audio_path: String):
-    if current_track == audio_path and music_player.playing:
-        return
-        
-    current_track = audio_path
-    var stream = load(audio_path)
-    music_player.stream = stream
-    music_player.play()
+	if (current_music_track == audio_path and music_player.playing):
+		return
+		
+	current_music_track = audio_path
+	var stream = load(audio_path)
+	
+	music_player.stream = stream
+	music_player.play() # Not checking music_enabled prior to playing so that music starts playing immediately after being enabled
+	if not music_enabled:
+		pause_music()
 func stop_music():
-    music_player.stop()
-    
+	music_player.stop()
+	
 func pause_music():
-    music_player.stream_paused = true
-    
+	music_player.stream_paused = true
+	
 func resume_music():
-    music_player.stream_paused = false
+	music_player.stream_paused = false
